5eb2df9f5a38a3b0e97ae531554e3bcb06399e64
Kyle Lemmon <kylelemmon@Kyles-MacBook-Air.local>
2021-03-01 22:48:23 -0800

    3_queries_group-by add group by and having sql assignment

---
+++ 3_queries_group-by/large_cohorts.sql
@@ -0,0 +1,9 @@
+
+
+SELECT cohorts.name as cohort_name, count(*) as student_count
+FROM students 
+JOIN cohorts 
+ON cohorts.id = students.cohort_id
+GROUP BY cohorts.name
+HAVING count(*) >= 18
+ORDER BY student_count ASC; 
